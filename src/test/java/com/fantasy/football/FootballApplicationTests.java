5dea18502a2c84bc77dfac289e8e21d7a76b7f84
Mohamed EL KHAYAT <elkhayat705@gmail.com>
2023-10-30 14:00:18 +0100

    first commit

---
+++ src/test/java/com/fantasy/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
