03532ae0d4c6a51d138d6158ed5a71b2b8412772
Mohamed EL KHAYAT <elkhayat705@gmail.com>
2023-11-09 19:58:01 +0100

    add test

---
+++ src/test/java/com/fantasy/football/repositories/PlayerRepositoriesTest.java
@@ -0,0 +1,48 @@
+package com.fantasy.football.repositories;
+
+import com.fantasy.football.entities.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PlayerRepositoriesTest {
+
+    @Autowired
+    PlayerRepositories underTest ;
+
+    @BeforeEach
+    void setUp() {
+
+        underTest.deleteAll();
+    }
+
+    @Test
+    void findByName() {
+        // Arrange
+        Player player1 = new Player();
+        player1.setName("Brahim Nakach");
+        underTest.save(player1);
+
+        Player player2 = new Player();
+        player2.setName("Mohamed Zrida");
+        underTest.save(player2);
+
+        // Act
+        List<Player> foundPlayers = underTest.findByName("Brahim Nakach");
+
+        // Assert
+        assertAll(
+                () -> assertNotNull(foundPlayers),
+                () -> assertEquals(1, foundPlayers.size()),
+                () -> assertEquals("Brahim Nakach", foundPlayers.get(0).getName())
+                // add more assertions as needed based on your Player class
+        );
+    }
+}
